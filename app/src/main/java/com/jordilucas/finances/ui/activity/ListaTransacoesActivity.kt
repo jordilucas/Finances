07f32ecc8f29781d1a2f69f403a73b7dbbc324f5
Jordi Lucas <jordi@joyjet.com>
2017-11-23 11:39:45 -0300

    Adicionado nova pasta res com arquivos de layout do curso alura kotlin

---
+++ app/src/main/java/com/jordilucas/finances/ui/activity/ListaTransacoesActivity.kt
@@ -0,0 +1,13 @@
+package com.jordilucas.finances.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.jordilucas.finances.R
+
+class ListaTransacoesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_transacoes)
+    }
+}
